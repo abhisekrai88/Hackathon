241d0ba9de35ef43bca530c7f41327cf5843d79a
Abhisek Rai <kesihba.iar@gmail.com>
2021-01-02 16:41:17 +0800

    edit

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { faTrash, faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
+library.add(faTrash, faEdit, faPlus);
+
+
 function App() {
   return (
     <div className="App">
