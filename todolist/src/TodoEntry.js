2f4d2ea1e396351e52b79b7ec5c0421b65e49fb4
Abhisek Rai <kesihba.iar@gmail.com>
2021-01-02 13:11:56 +0800

    added comments for explanation of each code

--- todolist/src/TodoEntry.js
+++ todolist/src/TodoEntry.js
@@ -2,12 +2,15 @@ import react, {Component} from "react";
 
 class TodoEntry extends Component{
     newTask(item) {
+        //using key attr (set by date.now() in todoentry.js) to make it easier to keep tracks of each of the elements.
         return <li key={item.key}>{item.text}</li>
     }
     render (){
         var todoItems = this.props.entry;
+        //using map to create an array by calling a function in order
         var itemsList = todoItems.map(this.newTask);
 
+        //to display elements on screen
         return (
             <ul classname="list">
                 {itemsList}
