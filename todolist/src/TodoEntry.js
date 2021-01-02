9e53b82e9c2820a8fbd9ba2926f9758914e491c7
Abhisek Rai <kesihba.iar@gmail.com>
2021-01-02 14:01:11 +0800

    starting on code to remove tasks

--- todolist/src/TodoEntry.js
+++ todolist/src/TodoEntry.js
@@ -1,10 +1,15 @@
 import react, {Component} from "react";
 
 class TodoEntry extends Component{
+    cons
     newTask(item) {
         //using key attr (set by date.now() in todoentry.js) to make it easier to keep tracks of each of the elements.
         return <li key={item.key}>{item.text}</li>
+        //removing todo items
+        return <li onClick={() => this.delete(item.key)} key = {item.key}>
+            {item.text}</li>
     }
+      
     render (){
         var todoItems = this.props.entry;
         //using map to create an array by calling a function in order
