651c67251fd0bc37623f22222314d22e3cc77a03
Abhisek Rai <kesihba.iar@gmail.com>
2021-01-02 11:18:37 +0800

    created todolist js file and imported to index js

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import TodoList from "./TodoList"
 
 var destination = document.querySelector("#container")
 
 ReactDOM.render(
- <div>Hello!</div>,
- destination
+  <div>
+    <TodoList/>
+  </div>,
+  destination
 );
